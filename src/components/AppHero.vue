40f7a98ebc27da4cdd615394037ab73a53a73b9e
FranPer23 <francescaprt@yahoo.com>
2023-04-12 18:22:56 +0200

    inserimento titolo e immagine

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <img class="img" src="../assets/img/landscapes2.jpg" alt="landscape">
+</template>
+
+<style>
+    .img {
+        height: 300px;
+        object-fit: cover;
+        box-shadow: 10px 10px 5px #dedede;
+        border-radius: 5px;
+    }
+</style>
